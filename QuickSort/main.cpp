1b2c71a4c4a1f88b8fe0d4be76159cd188479e24
KOCCA53 <KOCCA53@DESKTOP-SEUNGSO>
2021-12-14 14:02:41 +0900

    퀵 정렬 작성 중

--- QuickSort/main.cpp
+++ QuickSort/main.cpp
@@ -2,23 +2,30 @@
 #include <iostream>
 #include <algorithm>
 
+using namespace std;
+
 void QuickSort(int arr[], int low, int high)
 {
     if (low >= high)
     {
         return;
     }
-    
+    // 왼쪽과 오른쪽을 설정
     int left = low, right = low;
     int pivot = arr[high];
+    
     for (; right < high; right++)
     {
-        //if (arr[left])
-        //{
-        //}
+        // 피봇보다 오른쪽 포인터의 수가 작을 경우
+        if (arr[right] < pivot)
+        {
+            swap(arr[left++], arr[right]);
+        }
     }
+    swap(arr[left], pivot);
 
-
+    QuickSort(arr, low, left - 1);
+    QuickSort(arr, left + 1, high);
 }
 
 int main()
@@ -26,8 +33,9 @@ int main()
     //int arrMax, int SearchIndex;
     //scanf_s("%d %d", &arrMax, &SearchIndex);
 
-    int arr[10] = {};
+    int arr[5] = { 3, 4, 5, 2, 1 };
 
+    QuickSort(arr, 0, 4);
 
     system("pause");
     return 0;
